720cfb2825efd97233bdc9188af7733b7418382b
Mike Wright <mwright@guidewire.com>
2014-12-08 15:37:47 -0800

    express

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 var Faker = require('Faker');
+var express = require('express');
 var rName = Faker.Name.findName();
 var rEmail = Faker.Internet.email();
 console.log("rName=" + rName + ", rEmail=" + rEmail);
+var app = express();
+var port = process.env.PORT || 8080;
+var router = express.Router();
+router.get('/',function(req,res) {
+  res.send('hi');
+});
+app.use('/api', router);
+app.listen(port);
+console.log('listening on ' + port);
 
