8474f98ec4f071aeae8371c7cf027e50721a79ee
Mike Wright <mwright@guidewire.com>
2014-12-08 15:44:50 -0800

    save/restore json

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 var Faker = require('Faker');
 var express = require('express');
+var userSave = require('save')('user');
 var rName = Faker.Name.findName();
 var rEmail = Faker.Internet.email();
 console.log("rName=" + rName + ", rEmail=" + rEmail);
+
+userSave.create({name: rName});
+
 var app = express();
 var port = process.env.PORT || 8080;
 var router = express.Router();
 router.get('/',function(req,res) {
-  res.send('hi');
+  userSave.find({}, function(error, users) {
+     res.send(users);
+  });
 });
 app.use('/api', router);
 app.listen(port);
 console.log('listening on ' + port);
+
 
