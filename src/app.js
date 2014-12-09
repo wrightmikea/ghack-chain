2fa50b4fd448976a82653b325cc94a78d7b9c335
Mike Wright <wrightmikea@gmail.com>
2014-12-08 21:10:30 -0800

    npm install --save mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 var Faker = require('Faker');
 var express = require('express');
 var userSave = require('save')('user');
+var mongoose = require('mongoose');
+
 var rName = Faker.Name.findName();
 var rEmail = Faker.Internet.email();
 console.log("rName=" + rName + ", rEmail=" + rEmail);
+
+mongoose.connect('mongodb://localhost/chain');
+var Person = mongoose.model('Person', { name: String });
+var errHandler = function (err) {
+    if (err) {
+        console.log("errHandler(" + err + ')');
+    }
+};
+
+var person = new Person({ name: rName });
+person.save(errHandler);
 
 userSave.create({name: rName});
 
@@ -18,5 +31,3 @@ router.get('/',function(req,res) {
 app.use('/api', router);
 app.listen(port);
 console.log('listening on ' + port);
-
-
